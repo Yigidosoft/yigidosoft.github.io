073577bfd602e5743f235e7a34bdd1748dac10d4
Fatih Delice <2017141044@cumhuriyet.edu.tr>
2022-04-21 00:37:06 +0300

    test updated

--- test/resources/js/script.js
+++ test/resources/js/script.js
@@ -43,3 +43,33 @@ function openNav() {
 function closeNav() {
   document.getElementById("mobile").style.width = "0%";
 }
+
+
+//slider 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("image-content");
+  var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
