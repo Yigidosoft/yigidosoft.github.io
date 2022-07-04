2618209a38ce5b36c2f9d049a2c27abd09130827
Fatih Delice <2017141044@cumhuriyet.edu.tr>
2022-07-04 23:59:29 +0300

    update build for gh-pages

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -2,23 +2,29 @@ import React from 'react'
 import sponsor from '../resources/sponsors/sponsor.webp';
 
 export default function Sponsors() {
-  return (
-    <div className="sponsors" id="sponsors">
-        <div className="sponsor">
-            <a href="#"><img src={sponsor} /></a>
-        </div>
-        <div className="sponsor">
-            <a href="#"><img src={sponsor} /></a>
-        </div>
-        <div className="sponsor">
-            <a href="#"><img src={sponsor} /></a>
-        </div>
-        <div className="sponsor">
-            <a href="#"><img src={sponsor} /></a>
-        </div>
-        <div className="sponsor">
-            <a href="#"><img src={sponsor} /></a>
-        </div>
-    </div>
-  )
+    return (
+        <>
+            <div className="sponsors-title">
+                <div className="dioun" />
+                <h3>Partnered with</h3>
+            </div>
+            <div className="sponsors" id="sponsors">
+                <div className="sponsor">
+                    <a href="#"><img src={sponsor} /></a>
+                </div>
+                <div className="sponsor">
+                    <a href="#"><img src={sponsor} /></a>
+                </div>
+                <div className="sponsor">
+                    <a href="#"><img src={sponsor} /></a>
+                </div>
+                <div className="sponsor">
+                    <a href="#"><img src={sponsor} /></a>
+                </div>
+                <div className="sponsor">
+                    <a href="#"><img src={sponsor} /></a>
+                </div>
+            </div>
+        </>
+    )
 }
